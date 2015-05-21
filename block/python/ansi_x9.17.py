2efbc25d454cf0a55c994bd1f21326abb7937631
Aaron Toponce <aaron.toponce@gmail.com>
2015-05-21 17:09:08 -0600

    micro performance enhancement for the timestamp

--- block/python/ansi_x9.17.py
+++ block/python/ansi_x9.17.py
@@ -15,16 +15,14 @@ def sxor(s1, s2):
     return ''.join(chr(ord(a)^ord(b)) for a,b in zip(s1,s2))
 
 salt = '8bbb35cb1626f7c9ef291b29261acbf5' # for PBKDF2
+key = KDF.PBKDF2('11cf0edfc106350f9d81abe1538b3f20', salt, 16, 0)
+seed = KDF.PBKDF2('11462794b14c20f6b0896bac3a921485', salt, 16, 0)
 
 if args.key:
     key = KDF.PBKDF2(bytes(args.key), salt, 16, 0)
-else:
-    key = KDF.PBKDF2('11cf0edfc106350f9d81abe1538b3f20', salt, 16, 0)
 
 if args.seed:
     seed = KDF.PBKDF2(bytes(args.seed), salt, 16, 0)
-else:
-    seed = KDF.PBKDF2('11462794b14c20f6b0896bac3a921485', salt, 16, 0)
 
 if args.numbers:
     number = int(args.numbers)
@@ -35,10 +33,8 @@ aes = AES.new(key)
 
 # the actual ANSI X9.17 algorithm
 for i in range(0, number):
-    # 0 iterations guarantees high performance 16-byte output
-    # the security rests on the entropy of 'key', not iterating pbkdf2
-    date = KDF.PBKDF2(bytes(time.time()), salt, 16, 0)
-    temp = aes.encrypt(date)
+    date = repr(time.time())
+    temp = aes.encrypt(date.zfill(32))
     out = aes.encrypt(sxor(seed,temp))
     seed = aes.encrypt(sxor(out,temp))
     print(int(out.encode('hex'), 16))
