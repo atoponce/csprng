63e87e31db86edff3fcd0efa3ec750313ddafe33
Aaron Toponce <aaron.toponce@gmail.com>
2015-05-21 19:07:28 -0600

    micro performance improvement

--- stream/python/ansi_x9.17.py
+++ stream/python/ansi_x9.17.py
@@ -15,16 +15,14 @@ def sxor(s1, s2):
     return ''.join(chr(ord(a)^ord(b)) for a,b in zip(s1,s2))
 
 salt = '25f32d809f5a3b0a5c54650547ec67c6' # for PBKDF2
+key = KDF.PBKDF2('11cf0edfc106350f9d81abe1538b3f20', salt, 16, 0)
+seed = KDF.PBKDF2('807f0b5ca6725ad073a37f5abaf8de39', salt, 16, 0)
 
 if args.key:
     key = KDF.PBKDF2(bytes(args.key), salt, 16, 0)
-else:
-    key = KDF.PBKDF2('11cf0edfc106350f9d81abe1538b3f20', salt, 16, 0)
 
 if args.seed:
     seed = KDF.PBKDF2(bytes(args.seed), salt, 16, 0)
-else:
-    seed = KDF.PBKDF2('807f0b5ca6725ad073a37f5abaf8de39', salt, 16, 0)
 
 if args.numbers:
     number = int(args.numbers)
@@ -35,10 +33,8 @@ arc4 = ARC4.new(key)
 
 # the actual ANSI X9.17 algorithm
 for i in range(0, number):
-    # 0 iterations guarantees high performance 16-byte output
-    # the security rests on the entropy of 'key', not iterating pbkdf2
-    date = KDF.PBKDF2(bytes(time.time()), salt, 16, 0)
-    temp = arc4.encrypt(date)
+    date = repr(time.time()
+    temp = arc4.encrypt(date.zfill(32))
     out = arc4.encrypt(sxor(seed, temp))
     seed = arc4.encrypt(sxor(out, temp))
     print(int(out.encode('hex'), 16))
