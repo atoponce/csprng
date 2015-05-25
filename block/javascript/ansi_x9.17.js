b15760b6d00c79ebbd3a7ae31c41bea3d61b77ee
Aaron Toponce <aaron.toponce@gmail.com>
2015-05-24 21:17:12 -0600

    semi-working javascript

---
+++ block/javascript/ansi_x9.17.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+
+var BigNumber = require('bignumber');
+var crypto = require('crypto');
+var stdio = require('stdio');
+
+var opts = stdio.getopt({
+    'key': {key: 'k', args: 1, description: 'AES key.'},
+    'seed': {key: 's', args: 1, description: 'Starting seed.'},
+    'numbers': {key: 'n', args: 1, description: 'Quantity of random numbers.'}
+});
+
+function sxor(s1, s2) {
+    s1 = new Buffer(s1);
+    s2 = new Buffer(s2);
+    var xor = [];
+    for (var i=0; i<s1.length; i++) {
+        xor.push(s1[i]^s2[i]);
+    }
+    return new Buffer(xor);
+}
+
+function encrypt(hex) {
+    var aes = crypto.createCipher('aes128', key);
+    ct = aes.update(hex, 'utf8', 'hex') + aes.final('hex');
+    return ct;
+}
+
+var salt = 'a149e11d6b49590b9b394568c603c9c1'; // for pbkdf2
+var key = '63e007252ab90adf43fb0515bbb42c47';
+var seed = new Buffer('0c2edeada47381a6590a56f37bcd1ac7', 'hex');
+var number = 1;
+
+if(opts.key) key = crypto.pbkdf2Sync(opts.key, salt, 0, 16).toString('hex');
+if(opts.seed) seed = crypto.pbkdf2Sync(opts.seed, salt, 0, 16).toString('hex');
+if(opts.numbers) number = opts.numbers;
+
+for (var i=0; i<number; i++) {
+    var date = (new Date).getTime().toString();
+    console.log(date); 
+    var temp = new Buffer(encrypt(date), 'hex');
+    console.log(temp); 
+    var out = new Buffer(encrypt(sxor(seed, temp), 'hex'));
+    console.log(out); 
+    seed = new Buffer(encrypt(sxor(out, temp), 'hex'));
+}
