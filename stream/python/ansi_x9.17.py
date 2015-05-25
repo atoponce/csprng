e5c6e6b1c49530fbf9733f357624733ff62233b6
Aaron Toponce <aaron.toponce@gmail.com>
2015-05-25 07:19:51 -0600

    switch LSB and MSB for 64-bit quads

--- stream/python/ansi_x9.17.py
+++ stream/python/ansi_x9.17.py
@@ -39,4 +39,4 @@ for i in range(0, number):
     out = arc4.encrypt(sxor(seed, temp))
     seed = arc4.encrypt(sxor(out, temp))
     res = struct.unpack_from('QQ', out)
-    print(res[0]+res[1]*2**64)
+    print(res[0]*2**64+res[1])
