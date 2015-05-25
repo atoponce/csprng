43858aff1de2802cc4767a1b13e4bedefcf4a24d
Aaron Toponce <aaron.toponce@gmail.com>
2015-05-25 06:34:57 -0600

    finally! a working node.js implementation

--- block/javascript/ansi_x9.17.js
+++ block/javascript/ansi_x9.17.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-//var BigNumber = require('bignumber');
+var BigNumber = require('bignumber');
 var crypto = require('crypto');
 var stdio = require('stdio');
 
@@ -21,35 +21,27 @@ function xor(s1, s2) {
 }
 
 function encrypt(s, k) {
-    var aes = crypto.createCipher('aes128', k);
-    aes.setAutoPadding(false);
-    return aes.update(s) + aes.final();
+    var aes = crypto.createCipher('aes-128-ecb', k);
+    return aes.update(s);
 }
 
-// salt.length = 32
 var salt = 'a149e11d6b49590b9b394568c603c9c1'; // for pbkdf2
-// key.length = 16
 var key = crypto.pbkdf2Sync('63e007252ab90adf43fb0515bbb42c47', salt, 0, 16);
-// seed.length = 16
 var seed = new Buffer('0c2edeada47381a6590a56f37bcd1ac7', 'hex');
 var number = 1;
 
-if(opts.key) key = crypto.pbkdf2Sync(opts.key, salt, 0, 16).toString('hex');
-if(opts.seed) seed = crypto.pbkdf2Sync(opts.seed, salt, 0, 16).toString('hex');
+if(opts.key) key = crypto.pbkdf2Sync(opts.key, salt, 0, 16);
+if(opts.seed) seed = crypto.pbkdf2Sync(opts.seed, salt, 0, 16);
 if(opts.numbers) number = opts.numbers;
+
+// Almost never return exponential notation
+BigNumber.config({ EXPONENTIAL_AT: 1e+9 });
 
 for (var i=0; i<number; i++) {
     var time = (new Date).getTime().toString();
-    console.log("Time: "+time.toString('hex'));
-    var date = crypto.pbkdf2Sync(time, salt, 0, 16); // date.length = 16
-    console.log("Date: "+date.toString('hex'));
-    var temp = new Buffer(encrypt(date, key));   // temp.length = 70
-    console.log("Temp: "+temp.toString('hex'));
+    var date = crypto.pbkdf2Sync(time, salt, 0, 16);
+    var temp = new Buffer(encrypt(date, key));
     var out = new Buffer(encrypt(xor(seed, temp), key));
-    console.log("Out: "+out.toString('hex'));
     seed = new Buffer(encrypt(xor(out, temp), key));
-    console.log("Seed: "+seed.toString('hex'));
-
-    //console.log(new BigNumber(out.toString('hex'), 16).toString());
-
+    console.log(new BigNumber(out.toString('hex'), 16).toString());
 }
