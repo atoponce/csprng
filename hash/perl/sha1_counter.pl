5545c589a5154856049b068d946de5bec5665561
Aaron Toponce <aaron.toponce@gmail.com>
2015-05-25 21:57:38 -0600

    add python and perl sha-1 counter rngs

---
+++ hash/perl/sha1_counter.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+
+use Crypt::PBKDF2;      # requires libcrypt-pbkdf2-perl
+use Digest::SHA;
+use Getopt::Long qw(GetOptions);
+use Time::HiRes;
+
+my $k;  # user-supplied key
+my $n;  # user-supplied rounds
+my $h;  # display usage
+
+my $number;
+my $pbkdf2;
+
+my $res = GetOptions(
+    "k|key=s"     => \$k,
+    "n|numbers=i" => \$n,
+    "h|help"      => \$h,
+);
+
+my $salt = pack('H*', '6e447d92b87553a7f893499b184b9411');  # for Crypt::PBKDF2
+my $key = pack('H*', 'd9b251c8db1f88b58641260843a68c57');
+
+$pbkdf2 = Crypt::PBKDF2->new(
+    hash_class  => 'HMACSHA1',
+    # 0 iterations guarantees high performance 16-byte output
+    # the security rests on the entropy of $key, not iterating pbkdf2
+    iterations  => 0,
+    output_len  => 16,
+    salt_len    => 4,
+);
+
+if ($h) {
+    print "usage: aes_counter.pl [-h] [-k KEY] [-s SEED] [-n NUMBERS]
+
+SHA-1 in counter mode CSPRNG
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEY, --key KEY     SHA-1 key.
+  -n NUMBERS, --numbers NUMBERS
+                        Quantity of random numbers.
+";
+    exit;
+}
+
+$key = $pbkdf2->PBKDF2($k, $salt) if ($k);
+$number = ($n ? int($n) : 1);
+
+my $counter = Time::HiRes::time()*1000000;
+
+for (my $i=0; $i<$number; $i++) {
+    my $out = Digest::SHA::sha1($key . $counter);
+    print unpack("QQ", $out), "\n";
+    $counter++;
+}
---
+++ hash/python/sha1_counter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import argparse
+import struct
+import time
+from Crypto.Hash import SHA
+from Crypto.Protocol import KDF
+
+parser = argparse.ArgumentParser(description='SHA-1 in counter mode CSPRNG')
+parser.add_argument('-k','--key', help='SHA-1 key.')
+parser.add_argument('-n','--numbers', help='Quantity of random numbers.')
+args = parser.parse_args()
+
+salt = '957954e4e3cc45e5049f20bfad8a58f2' # for PBKDF2
+key = KDF.PBKDF2('faf72df86670f51512482d9ff0fb62f3', salt, 16, 0)
+number = 1
+
+if args.key:
+    key = KDF.PBKDF2(bytes(args.key), salt, 16, 0)
+
+if args.numbers:
+    number = int(args.numbers)
+
+counter = int(time.time())
+
+# SHA-1 in counter mode
+for i in range(0, number):
+    sha1 = SHA.new(key)
+    sha1.update(bytes(counter))
+    out = sha1.digest()
+    res = struct.unpack_from('QQ', out)
+    print res[0]*2**64+res[1]
+    counter += 1
